d5a6461bb8bb6773681ecdfd3de75c9915715e71
daniel <danielperaltamadriz@gmail.com>
2018-07-31 03:53:48 -0600

    Connection to DB-MySQL

--- LoginService/src/java/Service/LogInService.java
+++ LoginService/src/java/Service/LogInService.java
@@ -5,9 +5,12 @@
  */
 package Service;
 
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import java.sql.*;
+import javax.ws.rs.InternalServerErrorException;
 
 /**
  *
@@ -20,7 +23,35 @@ public class LogInService {
      * This is a sample web service operation
      */
     @WebMethod(operationName = "LoginUsuario")
-    public boolean loginUsuario(@WebParam(name = "ced") String ced, @WebParam(name = "pass") String pass) {
-        return ced.equals("1-0547-0587") && pass.equals("mipass");
+    public boolean loginUsuario(@WebParam(name = "ced") String ced, @WebParam(name = "pass") String pass) throws Exception {
+        System.out.println("Running");
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setDatabaseName("mis_kilos");
+        dataSource.setUser("root");
+        dataSource.setPassword("pass");
+        dataSource.setServerName("localhost");
+        boolean isUser = false;
+        try {
+            Connection conn = dataSource.getConnection();
+            Statement stmt = conn.createStatement();
+            String query = "SELECT * FROM user WHERE ced='" + ced + "' and pass='" + pass + "'";
+            System.out.format("Query: %s\n", query);
+            ResultSet rs = stmt.executeQuery(query);
+            if (rs.next()) {
+                String db_ced = rs.getString("ced");
+                String db_pass = rs.getString("pass");
+                System.out.format("%s, %s\n", db_ced, db_pass);
+                isUser = true;
+            } else {
+                System.out.println("usuario no encontrado!");
+            }
+             rs.close();
+            stmt.close();
+            conn.close();
+            return isUser;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new InternalServerErrorException();
+        } 
     }
 }
